20e7c56f8f2d5e24c5bc366a17b11d737f8cf431
locomite <raymond@midnight.com.au>
2020-09-01 13:47:27 +0800

    Changed probe offset for new bracket

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -975,7 +975,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -35, -41, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -35, -39, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
